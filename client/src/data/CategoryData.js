8b7186f6be888d0a65b8fd14a657c4317fa029e2
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2022-04-20 21:32:55 +0530

    clients-side added

---
+++ client/src/data/CategoryData.js
@@ -0,0 +1,64 @@
+// Category data
+
+export const Categories = [
+    {
+        id:1,
+        name:"BarChart",
+        description:"Let's visulaize the comparison of male and female reviews using Bar Chart",
+        tags:["art","design","artists","designers"],
+        demo:"/visualizebarchart",
+        
+    },
+    {
+        id:2,
+        name:"Polar Chart",
+        description:"Let's visulaize the comparison of male and female reviews using Polar Chart",
+        tags:["books","reference","reading","literature"],
+        
+        demo:"/polarchart",
+       
+        
+    },
+    {
+        id:3,
+        name:"LineChart",
+        description:"Let's visulaize the comparison of male and female reviews using Line Chart.",
+        tags:["books","reference","reading","literature"],
+        
+        demo:"/linechart",
+       
+        
+    },
+   
+    {
+        id:4,
+        name:"PieChart",
+        description:"Let's visulaize the comparison of male and female reviews using Pie Chart",
+        tags:["education","learning","teaching"],
+        
+        demo:"/piechart",
+        
+       
+    },
+    {
+        id:5,
+        name:"Doughnut Chart",
+        description:"Let's visulaize the comparison of male and female reviews using Doughnut",
+        tags:["communication","talk"],
+        
+        demo:"/doughnut",
+        
+    },
+    {
+        id:6,
+        name:"Radar Chart",
+        description:"Let's visulaize the comparison of male and female reviews using RadarChart",
+        tags:["communication","talk"],
+        
+        demo:"/radarchart",
+        
+    },
+    
+
+  
+] 
